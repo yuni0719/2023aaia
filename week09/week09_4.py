aea9597cc5b56457b2b574b2957e2c508264a7c7
yuni0719 <nicolewu0719@gmail.com>
2023-11-07 12:00:14 +0800

    week09

--- week09/week09_4.py
+++ week09/week09_4.py
@@ -1,11 +1,18 @@
 class Solution:
     def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+       #要小到大排序
+        a = arr
+        N = len(a)
 
-       a = arr
-       N = len(a)
-       for i in range(N-1):
-           d = a[1] - a[0] #基礎的距離
-           if a[i+1] - a[i] !=d: #距離不相等
+        for k in range(N):
+            for i in range(N-1):
+                if a[i+1] < a[i]:
+                    a[i], a[i+1] = a[i+1], a[i]
+
+        for i in range(N-1):
+            d = a[1] - a[0] #基礎的距離
+            if a[i+1] - a[i] !=d: #距離不相等
                 return False
+
 
         return True
