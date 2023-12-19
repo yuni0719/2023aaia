3a8f8c2c7d019c7242a6a99961f1e6196702150a
yuni0719 <nicolewu0719@gmail.com>
2023-12-19 11:55:08 +0800

    week15

---
+++ week15/week15_6a.py
@@ -0,0 +1,5 @@
+a,b,c,d = list(map(int, input().split() ))
+
+ans = (a-c) * (b-d)
+if ans<0: ans = -ans
+print(ans, end='')
---
+++ week15/week15_6b.py
@@ -0,0 +1,6 @@
+a, b = list(map(int, input().split() ))
+
+ans = a // b
+if a%b>0: ans += 1
+
+print(ans, end='')
